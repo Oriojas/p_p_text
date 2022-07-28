c18aa9269dc33a559f9ff963a84891bffd53a5ea
oscar50 <oscar@whaleandjaguar.co>
2022-07-27 20:20:48 -0500

    Se crea clase clean_text.py y el metodo c_numbers

---
+++ p_p_text/clean_text.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 27 20:08:55 2022
+
+@author: oscar
+"""
+
+class cleanText():
+    # to_do esta clase limpia el texto
+
+
+
+    def __init__(self, input_text: str) -> None:
+
+        # inicializo las variables
+        self.input_text = input_text
+
+
+    def c_numbers(self, not_clear: bool = True) -> str:
+        '''
+        Esta función elimina los numeros de un cadena de texto
+
+        Parameters
+        ----------
+        not_clear : bool, optional si no quiero eliminar los numeros
+            DESCRIPTION. The default is True.
+
+        Returns
+        -------
+        str
+            DESCRIPTION. texto sin numeros
+
+        '''
+
+        if not_clear:
+            c_numbers_pattern = re.compile(r"\@[a-zA-Z0-9\-\_]+\b")
+            out_put_txt = re.sub(c_numbers_pattern, " ", self.input_text)
+        else: out_put_txt = self.input_text
+
+        return out_put_txt
+
+    def c_mentios(self):
+
+        # este metodo borra las @
+
+    def c_emojis(self):
+
+        # este metodo bora :)
+
+    def c_url(self):
+
+        # este metodo borra las url
+
+    def c_hasgtags(self):
+
+        # borra #
+
+
+
