b8c37aaa37da11a367a88919aef8a2f06b39cc60
Miguel Pinto <zermaru72@gmail.com>
2022-07-28 21:07:06 -0500

    se agrego el esqueleto de la nueva clase stop_words

---
+++ p_p_text/clear_stop_words.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 20:55:51 2022
+
+@author: User
+"""
+
+import re
+
+stop_words =  #carga el archivo de las stop words
+
+def div_text(text):
+    
+    #vuelve al texto una lista (separa el texto)
+    
+def min_text(text):
+    
+    #vuelve a minusculas el texto
+    
+
+class stop_words():
+    
+    def __init__(self, input_text: str, stop_words: str ): -> None:
+        
+        self.input_text = input_text
+        self.stop_words = stop_words
+        
+        def clear_stop_words():
+            
+            #elimina las stop words
+            
+        def join_text():
+            
+            #vueleve al texto una cadena (une el texto)
+        
